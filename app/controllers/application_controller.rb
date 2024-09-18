2affb99f0bb5216d477804f252002ce740a1a8dd
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-09-18 12:58:17 +0530

    added controller endpoints for curd using blueprint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def default_url_options
+    { app_name: :ecommerce_app }
+  end
+
+  def health
+    render json: {"status": "ok"}, status: 200
+  end
 end
