4a201eb549a87ecbe7894f94fc38c58fac516d8c
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-09-18 19:42:18 +0530

    Update product_blueprint.rb

--- app/blueprints/product_blueprint.rb
+++ app/blueprints/product_blueprint.rb
@@ -1,4 +1,7 @@
 class ProductBlueprint < Blueprinter::Base
   identifier :id
   fields :name, :description, :price, :stock_quantity, :category_id
+  field :full_name do |product|
+    "DMART - #{product.name}"
+  end
 end
