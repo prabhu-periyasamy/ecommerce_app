cda799f78b64989454ec754ea72ecab3f6480d2a
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-09-19 14:08:26 +0530

    switched back to render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    puts UserBlueprint.render(@users)
+    render UserBlueprint.render(@users)
   end
 
   def show
