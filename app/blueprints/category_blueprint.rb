5fbb15da5de4fe26a83a1c445a5d094288b85d12
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-09-23 15:12:48 +0530

    updated categories endpoint

--- app/blueprints/category_blueprint.rb
+++ app/blueprints/category_blueprint.rb
@@ -2,5 +2,5 @@ class CategoryBlueprint < Blueprinter::Base
   identifier :id
   fields :name, :description
   # category has many products
-  association :products, blueprint: ProductBlueprint
+  association :products, blueprint: ProductBlueprint, view: :order_items_view
 end
