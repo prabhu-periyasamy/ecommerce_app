51a2b6e90f527766fc011e4e3b584317100e1074
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-10-06 15:49:43 +0530

    render partial by passing data to it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: UserBlueprint.render(@user, view: :normal)
+    # render json: UserBlueprint.render(@user, view: :normal)
   end
 
   def create
