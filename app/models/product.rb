a8f162a6e2d48cd7b6a475b012579a1b2b6659e6
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-12-03 16:45:43 +0530

    corrected polymorphic association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
   # polymorphic association
-  has_one :imageable, polymorphic: true
+  has_one :picture, as: :imageable
 
   scope :in_stock, -> { where('stock_quantity > 0') }
 end
