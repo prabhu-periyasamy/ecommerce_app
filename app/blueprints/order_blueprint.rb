26f0bb149ee600a99a8c88c95371e256f0ae6c5b
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2025-01-26 11:28:04 +0530

    added transformer

--- app/blueprints/order_blueprint.rb
+++ app/blueprints/order_blueprint.rb
@@ -1,5 +1,8 @@
 class OrderBlueprint < Blueprinter::Base
+  # Global level added transformers are not working need to check
+  # transform Transformers::AddCurrentDateTransformer
   view :normal do
+    transform Transformers::AddCurrentDateTransformer
     fields :order_date, :status, :total_amount
     association :order_items, blueprint: OrderItemBlueprint, view: :normal
   end
